2b66bc1911341be125d36d204ddc40e9004b5ecf
99blanc <t990421@naver.com>
2025-10-30 04:58:38 +0900

    [Feat] 예제 3-1 학습

---
+++ Masterpiece_4e/src/ch2n2/Const.java
@@ -0,0 +1,9 @@
+package ch2n2;
+
+public class Const {
+	public static void main(String[] args) {
+		final double PI = 3.141592;
+		
+		System.out.println(PI);
+	}
+}
---
+++ Masterpiece_4e/src/ch3n1/MainParameter.java
@@ -0,0 +1,11 @@
+package ch3n1;
+
+public class MainParameter {
+	public static void main(String[] args) {
+		String a = args[0];
+		String b = args[1];
+		String c = args[2];
+		
+		System.out.print(a + b + c);
+	}
+}
