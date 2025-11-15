264d93e4c1920c108fa4b0840cf6a3cb242ac744
99blanc <t990421@naver.com>
2025-11-15 19:49:12 +0900

    [Chore] 테스트를 위한 패키지와 클래스 생성

---
+++ Masterpiece_4e/src/chtest/Test.java
@@ -0,0 +1,19 @@
+package chtest;
+
+public class Test {
+	public static void main(String[] args) {
+		int option = 1;
+		
+		switch (option) {
+			case 1:
+				System.out.println("옵션 1");
+			case 2:
+				System.out.println("옵션 2");
+			case 3:
+				System.out.println("옵션 3");
+				break;
+			default:
+				System.out.println("해당 없음");
+		}
+	}
+}
